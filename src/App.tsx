4958e1f071c235d6ffebcf6de8c919017ab710e1
rubenbrandao <rubenavbrandao@gmail.com>
2023-12-06 19:28:32 +0000

    feat: inital logic, emojis constants and shuffle method

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import EMOJIS from "./constants/emojis";
+import { v4 as uuidv4 } from "uuid";
+
+type EmojiCard = {
+  id: string;
+  emoji: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [emojis, setEmojis] = useState<EmojiCard[]>([]);
+  const [turns, setTurns] = useState<number>(0);
+
+  const shuffle = () => {
+    const emojiCard: EmojiCard[] = EMOJIS.map((emoji) => ({
+      id: uuidv4(),
+      emoji: emoji,
+    }));
+
+    const shuffled = [...emojiCard, ...emojiCard].sort(
+      () => Math.random() - 0.5
+    );
+
+    setEmojis(shuffled);
+    setTurns(0);
+  };
+
+  console.log(emojis, turns);
 
   return (
     <>
@@ -18,9 +42,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={shuffle}>count</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +51,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
